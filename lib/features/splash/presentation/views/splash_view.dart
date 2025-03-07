b9ba3ae8d18d311fbb4b68afb892f7ed91bd8eb1
interesta22 <2albelasd@gmail.com>
2025-03-07 02:06:20 +0200

    First commit

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:bookly_app/features/splash/presentation/view%20models/splash_view_model.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
